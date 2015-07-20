11a88b398a76b5c2b9e6a1da709a26c2933a3599
Matt <matt@builtbyfuse.com>
2015-07-20 22:40:02 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+
+var app = angular.module('blog', []);
+
+    app.service("blogService", function($http, $q) {
+
+			// Return public API.
+			return({
+				getPosts: getPosts
+			});
+
+
+			// ---
+			// PUBLIC METHODS.
+			// ---
+
+
+			// I get all of the friends in the remote collection.
+			function getPosts() {
+
+				var request = $http({
+					method: "get",
+					url: "http://localhost:3000/posts",
+					params: {
+						action: "get"
+					}
+				});
+
+				return( request.then( handleSuccess, handleError ) );
+
+			}
+
+
+			// ---
+			// PRIVATE METHODS.
+			// ---
+
+
+			// I transform the error response, unwrapping the application dta from
+			// the API response payload.
+			function handleError( response ) {
+
+				// The API response from the server should be returned in a
+				// nomralized format. However, if the request was not handled by the
+				// server (or what not handles properly - ex. server error), then we
+				// may have to normalize it on our end, as best we can.
+				if (
+					! angular.isObject( response.data ) ||
+					! response.data.message
+					) {
+
+					return( $q.reject( "An unknown error occurred." ) );
+
+				}
+
+				// Otherwise, use expected error message.
+				return( $q.reject( response.data.message ) );
+
+			}
+
+
+			// I transform the successful response, unwrapping the application data
+			// from the API response payload.
+			function handleSuccess( response ) {
+
+				return( response.data );
+
+			}
+
+		}
+	);
+
+    app.controller('blogController', function ($scope, blogService) {
+        var blogController = this;
+
+        blogController.posts = [];
+
+        function applyRemoteData( posts ) {
+			blogController.posts = posts.posts;
+		}
+
+        blogController.loadPosts = function () {
+            blogService.getPosts().then(
+					function( posts ) {
+						applyRemoteData(posts);
+					}
+				);
+        }
+
+        blogController.loadPosts();
+    });
