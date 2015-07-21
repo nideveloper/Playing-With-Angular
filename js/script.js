70426b97f1dee7a0b4d7090a1ef548ba2a6d6772
Matt <matt@builtbyfuse.com>
2015-07-21 21:08:04 +0100

    Changed code to point to express proxy

--- js/script.js
+++ js/script.js
@@ -1,123 +1,127 @@
 
 var app = angular.module('blog', []);
 
-    app.service("blogService", function($http, $q) {
+app.config(function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
 
-			// Return public API.
-			return({
-				getPosts: getPosts,
-                getTweets: getTweets
-			});
+app.service("blogService", function($http, $q) {
+
+	// Return public API.
+	return({
+		getPosts: getPosts,
+        getTweets: getTweets
+	});
 
 
-			// ---
-			// PUBLIC METHODS.
-			// ---
+	// ---
+	// PUBLIC METHODS.
+	// ---
 
 
-			// I get all of the friends in the remote collection.
-			function getPosts() {
+	// I get all of the friends in the remote collection.
+	function getPosts() {
 
-				var request = $http({
-					method: "get",
-					url: "http://localhost:3000/api/posts",
-					params: {
-					}
-				});
-
-				return( request.then( handleSuccess, handleError ) );
-
+		var request = $http({
+			method: "get",
+			url: "https://enigmatic-headland-6062.herokuapp.com/api/posts",
+			params: {
 			}
+		});
 
-            function getTweets() {
+		return( request.then( handleSuccess, handleError ) );
 
-				var request = $http({
-					method: "get",
-					url: "http://localhost:3000/twitter/nideveloper",
-					params: {
-					}
-				});
+	}
 
-				return( request.then( handleSuccess, handleError ) );
+    function getTweets() {
 
+		var request = $http({
+			method: "get",
+			url: "https://enigmatic-headland-6062.herokuapp.com/twitter/nideveloper",
+			params: {
 			}
+		});
+
+		return( request.then( handleSuccess, handleError ) );
+
+	}
 
 
-			// ---
-			// PRIVATE METHODS.
-			// ---
+	// ---
+	// PRIVATE METHODS.
+	// ---
 
 
-			// I transform the error response, unwrapping the application dta from
-			// the API response payload.
-			function handleError( response ) {
+	// I transform the error response, unwrapping the application dta from
+	// the API response payload.
+	function handleError( response ) {
 
-				// The API response from the server should be returned in a
-				// nomralized format. However, if the request was not handled by the
-				// server (or what not handles properly - ex. server error), then we
-				// may have to normalize it on our end, as best we can.
-				if (
-					! angular.isObject( response.data ) ||
-					! response.data.message
-					) {
+		// The API response from the server should be returned in a
+		// nomralized format. However, if the request was not handled by the
+		// server (or what not handles properly - ex. server error), then we
+		// may have to normalize it on our end, as best we can.
+		if (
+			! angular.isObject( response.data ) ||
+			! response.data.message
+			) {
 
-					return( $q.reject( "An unknown error occurred." ) );
+			return( $q.reject( "An unknown error occurred." ) );
 
+		}
+
+		// Otherwise, use expected error message.
+		return( $q.reject( response.data.message ) );
+
+	}
+
+
+	// I transform the successful response, unwrapping the application data
+	// from the API response payload.
+	function handleSuccess( response ) {
+
+		return( response.data );
+
+	}
+
+});
+
+app.controller('blogController', function ($scope, blogService) {
+    var blogController = this;
+
+    blogController.posts = [];
+
+    function applyRemoteData( posts ) {
+		blogController.posts = posts.posts;
+	}
+
+    blogController.loadPosts = function () {
+        blogService.getPosts().then(
+				function( posts ) {
+					applyRemoteData(posts);
 				}
+			);
+    }
 
-				// Otherwise, use expected error message.
-				return( $q.reject( response.data.message ) );
+    blogController.loadPosts();
+});
 
-			}
+app.controller('tweetController', function ($scope, blogService) {
+    var tweetController = this;
 
+    tweetController.tweets = [];
 
-			// I transform the successful response, unwrapping the application data
-			// from the API response payload.
-			function handleSuccess( response ) {
+    function applyRemoteData( tweets ) {
+		tweetController.tweets = tweets;
+	}
 
-				return( response.data );
+    tweetController.loadTweets = function () {
+        blogService.getTweets().then(
+				function( tweets ) {
+					applyRemoteData(tweets);
+				}
+			);
+    }
 
-			}
-
-		}
-	);
-
-    app.controller('blogController', function ($scope, blogService) {
-        var blogController = this;
-
-        blogController.posts = [];
-
-        function applyRemoteData( posts ) {
-			blogController.posts = posts.posts;
-		}
-
-        blogController.loadPosts = function () {
-            blogService.getPosts().then(
-					function( posts ) {
-						applyRemoteData(posts);
-					}
-				);
-        }
-
-        blogController.loadPosts();
-    });
-
-    app.controller('tweetController', function ($scope, blogService) {
-        var tweetController = this;
-
-        tweetController.tweets = [];
-
-        function applyRemoteData( tweets ) {
-			tweetController.tweets = tweets;
-		}
-
-        tweetController.loadTweets = function () {
-            blogService.getTweets().then(
-					function( tweets ) {
-						applyRemoteData(tweets);
-					}
-				);
-        }
-
-        tweetController.loadTweets();
-    });
+    tweetController.loadTweets();
+});
